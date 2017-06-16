1da5451e945422e201dfb86bdc2d7c6f44f1b33d
veeramarni <mvasireddy@cdmbase.com>
2017-06-16 08:42:11 -0400

    added clientLogger

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { injectable, inject, Container } from 'inversify'
 import {ConsoleLogger,Logger, IConsoleLoggerSettings,getConsoleStream, getLoggerOptions, createLogger, makeLogger, LogstashAmqpLogger} from "../index";
+import { ClientLogger } from '../clientLogger'
 
 function testLogger(logger: Logger, msg: string) {
   logger.trace(msg);
@@ -231,4 +232,39 @@ describe("Console Logger for child classes", () => {
     testClass.test();
   });
 
+})
+
+describe("Client Logger", () => {
+  it("should be able to create a TRACE instance", () => {
+    const settings: IConsoleLoggerSettings = {
+      mode:"long",
+      level: "trace"
+    }
+
+    const logger: Logger = ConsoleLogger.create("TestLog", settings);
+    expect(logger).to.be.not.undefined;
+    expect(logger).to.be.not.null;
+    testLogger(logger, settings.level)
+  });
+
+  it("should be able to create an instance without settings", () => {
+    const logger: Logger = ConsoleLogger.create("TestLog");
+    expect(logger).to.be.not.undefined;
+    expect(logger).to.be.not.null;
+    testLogger(logger, "INFO")
+  });
+
+  it("should be able to create an instance with 2 streams at different levels", () => {
+    const logger: Logger = createMultiLogger("TestLog", {mode: "long", level: "trace"}, {mode: "short", level: "warn"});
+    expect(logger).to.be.not.undefined;
+    expect(logger).to.be.not.null;
+    testLogger(logger, "long/trace + short/warn")
+  });
+
+  it("should be able to create an instance with 2 streams at different levels", () => {
+    const logger: Logger = createMultiLogger("TestLog", {mode: "long", level: "warn"}, {mode: "short", level: "trace"});
+    expect(logger).to.be.not.undefined;
+    expect(logger).to.be.not.null;
+    testLogger(logger, "long/warn + short/trace")
+  });
 })
