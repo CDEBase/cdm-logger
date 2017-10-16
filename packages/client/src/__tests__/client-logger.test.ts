d701b40b99e4dc16ae10fb026b541d9c8cb51c32
Lyuda Dzyubinska <lyuda.dzyubinska@gmail.com>
2017-10-16 23:17:55 +0300

    connect jsDom to jest

--- packages/client/src/__tests__/client-logger.test.ts
+++ packages/client/src/__tests__/client-logger.test.ts
@@ -5,6 +5,11 @@ import { ClientLogger, getConsoleStream } from '../client-logger'
 import { ILoggerSettings } from '../interfaces';
 import 'jest'
 
+import * as jsdom from 'jsdom';
+const { JSDOM } = jsdom;
+
+const $ = require('jquery');
+
 function testLogger(logger: Logger, msg: string) {
   logger.trace(msg);
   logger.debug(msg);
@@ -19,22 +24,62 @@ function createMultiLogger(name: string | Object, settings1: any, settings2: any
 }
 
 describe("Client Logger", () => {
+  // it("should be able to create a TRACE instance", () => {
+  //   const settings: ILoggerSettings = {
+  //     level: "trace"
+  //   }
+  //
+  //   const logger: Logger = ClientLogger.create("TestLog", settings);
+  //   expect(logger).not.toBeUndefined;
+  //   expect(logger).not.toBeNull;
+  //   testLogger(logger, settings.level)
+  // });
+  //
+  // it("should be able to create an instance without settings", () => {
+  //   const logger: Logger = ClientLogger.create("TestLog");
+  //   expect(logger).not.toBeUndefined;
+  //   expect(logger).not.toBeNull;
+  //   testLogger(logger, "INFO")
+  // });
+
+  // it("should be able to create a INFO instance", () => {
+  //   const settings: ILoggerSettings = {
+  //     level: "info"
+  //   }
+  //
+  //   const logger: Logger = ClientLogger.create("TestLog", settings);
+  //   expect(logger).not.toBeUndefined;
+  //   expect(logger).not.toBeNull;
+  //   testLogger(logger, settings.level)
+  // });
+  //
+  // it("should be able to create a child instance", () => {
+  //   const logger: Logger = ClientLogger.create('FullStack', { level: 'trace' });
+  //   expect(logger).not.toBeUndefined;
+  //   expect(logger).not.toBeNull;
+  //
+  //   const newlog = logger.child({ childName: 'UIController' });
+  //   testLogger(newlog,  "Trace")
+  // });
+
   it("should be able to create a TRACE instance", () => {
-    const settings: ILoggerSettings = {
-      level: "trace"
-    }
 
-    const logger: Logger = ClientLogger.create("TestLog", settings);
-    expect(logger).not.toBeUndefined;
-    expect(logger).not.toBeNull;
-    testLogger(logger, settings.level)
-  });
+    document.body.innerHTML =
+    '<div>' +
+    '  <span id="username" />' +
+    '  <button id="button">example<button>' +
+    '</div>';
 
-  it("should be able to create an instance without settings", () => {
     const logger: Logger = ClientLogger.create("TestLog");
-    expect(logger).not.toBeUndefined;
-    expect(logger).not.toBeNull;
-    testLogger(logger, "INFO")
+      $('#button').click(() => {
+        console.log('in', logger)
+        expect(logger).not.toBeUndefined;
+        expect(logger).not.toBeNull;
+        logger.trace('TestLog');
+      });
+
+      $('#button').click();
+
   });
 
   // it("should be able to create an instance with 2 streams at different levels", () => {
