5ec9dbc3e8530f768b18a79d55fe7cc30856f60f
veera <marni.srikanth@gmail.com>
2025-05-12 10:29:04 -0400

    update packages

--- packages/server/src/moleculer.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Re-export the CdmMoleculerLogger class
- */
-
-// Import default export from the file
-import MoleculerLogger from './adapters/moleculer-logger';
-
-// Export as named export
-export { MoleculerLogger }; 
